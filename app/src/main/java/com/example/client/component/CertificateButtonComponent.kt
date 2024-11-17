817c6413ce200432238f9b1e0a890d04f39c60fc
aiken516 <jimmy000516@naver.com>
2024-11-17 21:25:17 +0900

    feature : 컴포넌트 4종 추가
    버튼, 자격증 버튼, 페이지 인덱스, 검색 컴포넌트 추가

---
+++ app/src/main/java/com/example/client/component/CertificateButtonComponent.kt
@@ -0,0 +1,96 @@
+package com.example.client.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.client.R
+
+@Composable
+fun CertificateButtonComponent(
+    certificateDepartment: String,
+    certificateName: String,
+    date: String,
+    onClick: () -> Unit
+) {
+    var isOverlayVisible by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .padding(8.dp)
+            .height(126.dp)
+            .width(155.dp)
+            .shadow(8.dp, RoundedCornerShape(24.dp))
+            .background(color = Color(0xFF8FA86A), shape = RoundedCornerShape(20.dp))
+            .clickable {
+                if (isOverlayVisible) { onClick() }
+                isOverlayVisible = !isOverlayVisible
+                       },
+        contentAlignment = Alignment.TopStart
+    ) {
+        // 기본 내용
+        Column(
+            modifier = Modifier.padding(13.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = certificateDepartment,
+                color = Color(0xFFDCF5B5),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = certificateName,
+                color = Color(0xFFFFFBDC),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Row(
+                verticalAlignment = Alignment.Bottom
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.icon_certificate),
+                    contentDescription = null,
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Text(
+                    text = date,
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+
+        if (isOverlayVisible) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = Color(0xAA000000), shape = RoundedCornerShape(8.dp))
+            ) {
+                Text(
+                    text = "관련 공고\n확인하기",
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/client/component/SearchComponent.kt
@@ -0,0 +1,102 @@
+package com.example.client.component
+
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import com.example.client.R
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.ui.text.input.ImeAction
+
+@Composable
+fun SearchComponent(
+    searchPlaceholderText: String,
+    onClickCancel: () -> Unit,
+    onSearch: (String) -> Unit
+) {
+    var searchText by remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xAA000000))
+    ) {
+
+        Box(
+            modifier = Modifier
+                .padding(8.dp)
+                .height(170.dp)
+                .width(348.dp)
+                .background(color = Color.White, shape = RoundedCornerShape(12.dp))
+                .border(2.dp, Color.LightGray, shape = RoundedCornerShape(8.dp)),
+            contentAlignment = Alignment.TopStart
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.icon_cancel),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clickable {
+                        onClickCancel()
+                    }
+                    .align(Alignment.TopEnd)
+            )
+
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Spacer(modifier = Modifier.height(20.dp))
+
+                OutlinedTextField(
+                    value = searchText,
+                    onValueChange = { searchText = it },
+                    placeholder = { Text(searchPlaceholderText) },
+                    modifier = Modifier
+                        .height(52.dp)
+                        .width(264.dp)
+                        .background(Color.LightGray, shape = RoundedCornerShape(8.dp)),
+                    colors = TextFieldDefaults.colors(
+                        unfocusedContainerColor = Color.White,
+                        focusedContainerColor = Color.White,
+                        unfocusedIndicatorColor = Color.LightGray,
+                        focusedIndicatorColor = Color.LightGray
+                    ),
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            onSearch(searchText)
+                        }
+                    )
+                )
+            }
+        }
+    }
+}

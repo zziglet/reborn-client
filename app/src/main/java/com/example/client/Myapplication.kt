15f5f5165264028ef3f3627aea0af8cb45d81f85
zziglet <jjw04033@konkuk.ac.kr>
2024-11-27 23:46:38 +0900

    fix: fix conflict(main)

--- app/src/main/java/com/example/client/Myapplication.kt
+++ app/src/main/java/com/example/client/Myapplication.kt
@@ -4,11 +4,11 @@ import android.app.Application
 import com.android.volley.BuildConfig
 import com.kakao.sdk.common.KakaoSdk
 
-//private const val APP_KEY = BuildConfig.KAKAO_API_KEY
+private val APP_KEY = com.example.client.BuildConfig.KAKAO_API_KEY
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         // Kakao SDK 초기화
-        //KakaoSdk.init(this, "APP_KEY")
+        KakaoSdk.init(this, "APP_KEY")
     }
 }
--- app/src/main/java/com/example/client/domain/KakaoLogin.kt
+++ app/src/main/java/com/example/client/domain/KakaoLogin.kt
@@ -23,7 +23,7 @@ interface ApiService {
 }
 
 object RetrofitClient {
-    private val BASE_URL = BuildConfig.BASE_URL // 서버 URL
+    private val BASE_URL = com.example.client.BuildConfig.BASE_URL // 서버 URL
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
